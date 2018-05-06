61af657b3e581fae04797ddf680146c0b546a117
lakshmiNdevulapalli <baludevulapalli@gmail.com>
2018-05-05 23:16:02 -0500

    Updated DriverRepository
    Updated DriverRepository

--- src/main/java/com/ezshipp/api/repositories/DriverRepository.java
+++ src/main/java/com/ezshipp/api/repositories/DriverRepository.java
@@ -2,9 +2,20 @@ package com.ezshipp.api.repositories;
 
 import com.ezshipp.api.document.Driver;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
 
 /**
  * Created by srinivasseri on 2/16/18.
  */
 public interface DriverRepository extends MongoRepository<Driver, String> {
+    Optional<Driver> findDriverBy_id(String id);
+
+    @Query("{ 'driverseqId' : { $regex: ?0 } }")
+    Driver findByDriverseqId(String regexp);
 }
+
+    /*@Override
+    Optional<Driver> findById(String s);
+    */
